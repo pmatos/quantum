00f6789e12ada411973dd5a468eab74eef6a8a93
Paulo Matos <pmatos@linki.tools>
2019-10-24 23:04:44 +0200

    Implemented sched-getaffinity and sched-setaffinity

--- quantum/posix.rkt
+++ quantum/posix.rkt
@@ -9,7 +9,9 @@
 (provide
  (c:contract-out
   [pagesize exact-positive-integer?]
-  [read-current-memory-use (c:-> exact-positive-integer? exact-positive-integer?)]))
+  [read-current-memory-use (c:-> exact-positive-integer? exact-positive-integer?)]
+  [sched-getaffinity (c:-> exact-nonnegative-integer? exact-nonnegative-integer?)]
+  [sched-setaffinity (c:-> exact-nonnegative-integer? exact-nonnegative-integer? void?)]))
 
 ;; ---------------------------------------------------------------------------------------------------
 
@@ -32,6 +34,25 @@
 ; Access to sched_ functions
 ;
 
+(define _pid _ulong)
+(define _cpu_set_t _ulong)
 
-(define sched_getaffinity (get-ffi-obj "sched_getaffinity" #false (_fun _pid _size_t _cpu_set_t -> _int)))
-                               
+(define (check v who)
+  (unless (zero? v)
+    (error who "failed: ~a" v)))
+
+(define _sched_getaffinity (get-ffi-obj "sched_getaffinity" #false
+                                        (_fun _pid _size (o : (_ptr o _cpu_set_t))
+                                              -> (r : _int)
+                                              -> (begin0 o (check r 'sched_getaffinity)))))
+
+(define _sched_setaffinity (get-ffi-obj "sched_setaffinity" #false
+                                        (_fun _pid _size (i : (_ptr i _cpu_set_t))
+                                              -> (r : _int)
+                                              -> (check r 'sched_setaffinity))))
+; external sched_getaffinity
+(define (sched-getaffinity pid)
+  (_sched_getaffinity pid (ctype-sizeof _cpu_set_t)))
+
+(define (sched-setaffinity pid mask)
+  (_sched_setaffinity pid (ctype-sizeof _cpu_set_t) mask))
